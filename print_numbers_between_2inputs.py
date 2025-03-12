ba1f2f69c7fef5e020033b829810b8e96fe73884
Linden <lindenpowell218@gmail.com>
2025-03-12 21:38:20 +0800

    Placed a comment for prog10batch2

--- print_numbers_between_2inputs.py
+++ print_numbers_between_2inputs.py
@@ -3,7 +3,11 @@
 
 num1 = int(input("First number: "))
 num2 = int(input("Second number: "))
+
+# Adds 1 point to the smaller number until it gets to the value of the bigger number.
 if num1 < num2:
     print(num1, list(range(num1 + 1, num2)), num2)
 else:
     print(num2, list(range(num2 + 1, num1)), num1)
+
+# Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
