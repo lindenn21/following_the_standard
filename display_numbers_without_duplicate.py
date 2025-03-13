2c37b3f0d029a029f113b6aea7a85d12fc27ed15
Linden <lindenpowell218@gmail.com>
2025-03-13 16:13:56 +0800

    Prog1 of Batch 3 added, trying stuff out

---
+++ display_numbers_without_duplicate.py
@@ -0,0 +1,29 @@
+# Ask user to input 10 numbers
+# Print numbers that do not have a duplicate
+
+def unique_num():
+    number = [ ]
+
+    for i in range(10):
+        num = int(input(f"Enter number: "))
+        number.append(num)
+
+
+unique_num()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
