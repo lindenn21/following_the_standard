05d0c17f3091acb78b3830f5ccbd1b0ca1426363
Linden <lindenpowell218@gmail.com>
2025-03-13 21:24:03 +0800

    Finished prog1batch3

--- display_unique_num.py
+++ display_unique_num.py
@@ -1,15 +1,14 @@
 # While loop: Input numbers
 # Display numbers with NO duplicate
 
+
 number = []
 
 for i in range(10):
     num = int(input(f"Enter number {i + 1}: "))
     number.append(num)
+    unique = [num for num in number if number.count(num) == 1]
 
-    if number.count(num) > 1:
-        number.remove(num)
-
-print(number)
+    print(unique)
 
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
