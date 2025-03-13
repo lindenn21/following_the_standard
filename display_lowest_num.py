43cf813a0ca9ffd1ce181286ef6d2976095a785b
Linden <lindenpowell218@gmail.com>
2025-03-13 17:24:26 +0800

    added prog4 of batch 3

---
+++ display_lowest_num.py
@@ -0,0 +1,19 @@
+# While loop, ask user to input until invalid
+# Display the lowest number
+
+def small():
+    number = []
+
+    while True:
+        try:
+            num = int(input("Enter number: "))
+            number.append(num)
+
+            smallest = min(number)
+            print(smallest)
+        except ValueError:
+            print("Invalid")
+small()
+
+# Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+
