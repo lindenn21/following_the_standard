55caded2799f7801d4e2cb932299d12569015a4b
Linden <lindenpowell218@gmail.com>
2025-03-13 17:34:14 +0800

    Added prog 5 of batch 3: sort not done

---
+++ display_lowest_to_highest.py
@@ -0,0 +1,16 @@
+# While loop asking to input num
+# Display inputted numbers from lowest to highest
+
+def smallest_highest():
+    number = []
+
+    while True:
+        num = float(input("Enter a number: "))
+        number.append(num)
+
+        sort(number)
+        print(number)
+
+smallest_highest()
+
+# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
