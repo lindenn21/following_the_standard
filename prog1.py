19adbc9769a8e9917167588a07bdf8c5c547ab73
Linden <lindenpowell218@gmail.com>
2025-03-13 17:49:55 +0800

    Trying the func used in prog3 batch3 for prog1: number.count

---
+++ prog1.py
@@ -0,0 +1,12 @@
+# While loop: Input numbers
+# Display numbers with NO duplicate
+
+number = []
+
+for i in range(10):
+    num = int(input(f"Enter number {i + 1}: "))
+    number.append(num)
+
+    if number.count(num) > 1:
+
+# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
